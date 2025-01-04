46b44014274a54f2b26fd5c92fc98fbd79c91add
Tango <tangzhenzhi@dreame.tech>
2025-01-04 13:45:22 +0800

    Initial Commit

---
+++ IOTClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOTClient
+//
+//  Created by Tang Tango on 2025/1/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
